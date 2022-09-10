17b6f99510e898363f8db4a06103d2813d09713f
tkoba0410 <tkoba2@nifty.com>
2022-09-10 12:44:47 +0900

    csv

--- getohlcpy/getohlcpy.py
+++ getohlcpy/getohlcpy.py
@@ -71,7 +71,6 @@ def csv_merge(
         df: pd.DataFrame,
         cashe_file: str) -> pd.DataFrame:
     df_cashe = pd.read_csv(cashe_file, header=0, index_col=0, parse_dates=True)
-    df_cashe.to_csv('df_cashe.csv')
     df_diff = df[~(df.index).isin(df_cashe.index[:-1])]
     df_result = pd.concat([df_cashe, df_diff], axis=0)
     return df_result
